efc943297fe2ec7dec88d54e8072ef2b28349f5a
Aleksandr Serebrov <sera@ciklum.com>
2016-12-12 11:20:11 +0200

    Added test for edit entry

--- flaskr_tests.py
+++ flaskr_tests.py
@@ -5,52 +5,71 @@ import tempfile
 
 class FlaskrTestCase(unittest.TestCase):
 
-	def setUp(self):
-		self.db_fd, flaskr.app.config['DATABASE'] = tempfile.mkstemp()
-		flaskr.app.config['TESTING'] = True
-		self.app = flaskr.app.test_client()
-		with flaskr.app.app_context():
-			flaskr.init_db()
+    def setUp(self):
+        self.db_fd, flaskr.app.config['DATABASE'] = tempfile.mkstemp()
+        flaskr.app.config['TESTING'] = True
+        self.app = flaskr.app.test_client()
+        with flaskr.app.app_context():
+            flaskr.init_db()
 
-	def tearDown(self):
-		os.close(self.db_fd)
-		os.unlink(flaskr.app.config['DATABASE'])
+    def tearDown(self):
+        os.close(self.db_fd)
+        os.unlink(flaskr.app.config['DATABASE'])
 
-	def test_empty_db(self):
-		rv = self.app.get('/')
-		assert b'No entries here so far' in rv.data
+    def test_empty_db(self):
+        rv = self.app.get('/')
+        assert b'No entries here so far' in rv.data
 
-	def login(self, username, password):
-		return self.app.post('/login', data=dict(
-			username=username,
-			password=password
-			), follow_redirects=True)
+    def login(self, username, password):
+        return self.app.post('/login', data=dict(
+            username=username,
+            password=password
+            ), follow_redirects=True)
 
-	def logout(self):
-		return self.app.get('logout', follow_redirects=True)
+    def logout(self):
+        return self.app.get('logout', follow_redirects=True)
 
-	def test_login_logout(self):
-		rv = self.login('admin','default')
-		assert b'You were logged in' in rv.data
-		
-		rv = self.logout()
-		assert b'You were logged out' in rv.data
+    def test_login_logout(self):
+        rv = self.login('admin','default')
+        assert b'You were logged in' in rv.data
+        
+        rv = self.logout()
+        assert b'You were logged out' in rv.data
 
-		rv = self.login('adminx','default')
-		assert b'Invalid username' in rv.data
-		
-		rv = self.login('admin','defaultx')
-		assert b'Invalid password' in rv.data
+        rv = self.login('adminx','default')
+        assert b'Invalid username' in rv.data
+        
+        rv = self.login('admin','defaultx')
+        assert b'Invalid password' in rv.data
 
-	def test_messages(self):
-		self.login('admin', 'default')
-		rv = self.app.post('/add', data=dict(
-			title='<Hello>',
-			text='<strong>HTML</strong> allowed here'
-			), follow_redirects=True)
-		assert b'No entries here so far' not in rv.data
-		assert b'&lt;Hello&gt;' in rv.data
-		assert b'<strong>HTML</strong> allowed here' in rv.data
+    def test_messages(self):
+        self.login('admin', 'default')
+        rv = self.app.post(
+            '/add',
+            data=dict(title='<Hello>',
+                      text='<strong>HTML</strong> allowed here'),
+            follow_redirects=True)
+        assert b'No entries here so far' not in rv.data
+        assert b'&lt;Hello&gt;' in rv.data
+        assert b'<strong>HTML</strong> allowed here' in rv.data
+
+    def test_edit(self):
+        self.login('admin', 'default')
+        self.app.post(
+            '/add',
+            data=dict(title='Hello',
+                      text='HTML allowed here'),
+            follow_redirects=True)
+        self.app.get('/entry=1', follow_redirects=True)
+        rv = self.app.post(
+            '/entry=1', 
+            data = dict(title='Bye',
+                        text='Nothing is allowed here'),
+            follow_redirects=True)
+        assert b'HTML' not in rv.data
+        assert b'Entry was successfuly edited' in rv.data
+        assert b'Bye' in rv.data
+
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
